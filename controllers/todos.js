8ec98c6edbc9cde3250f575c511fd01613289ad6
Margit Viigi <margit.viigi@voco.ee>
2024-12-13 17:00:22 +0200

    Todo patch and delete

--- controllers/todos.js
+++ controllers/todos.js
@@ -15,5 +15,51 @@ class todoController {
     getTodos(req, res){
         res.json ({tasks: this.TODOS})
     } 
+
+    updateTodo(req, res){
+        try{
+        const todoId = req.params.id 
+        const updatedTask = req.body.task
+
+        const todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId)
+
+        if(todoIndex < 0){
+            res.json({
+                message: 'Could not find todo with such index.'
+            } )
+            throw new Error('Could not find todo')
+        } 
+        this.TODOS[todoIndex] = new Todo (this.TODOS[todoIndex].id, updatedTask)
+        res.json({
+            message: 'todo is updated',
+            updatedTask: this.TODOS[todoIndex] 
+      
+        })
+  } catch (error){
+    console.log(error.message)
+  } 
+    } 
+    deleteTodo(req, res){
+        try{
+        const todoId = req.params.id 
+        const updatedTask = req.body.task
+
+        const todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId)
+
+        if(todoIndex < 0){
+            res.json({
+                message: 'Could not find todo with such index.'
+            } )
+            throw new Error('Could not find todo')
+        } 
+        this.TODOS.splice(todoIndex, 1)
+        res.json({
+            message: 'todo is deleted',
+      
+        })
+  } catch (error){
+    console.log(error.message)
+  } 
+    } 
 } 
 export const TodoController = new todoController()
